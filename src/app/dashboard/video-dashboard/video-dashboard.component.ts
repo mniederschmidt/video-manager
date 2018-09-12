6dd74c3b929829548642cabe8cee5e1ad71698ee
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2018-09-12 15:39:39 -0500

    Initial commit

---
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vm-video-dashboard',
+  templateUrl: './video-dashboard.component.html',
+  styleUrls: ['./video-dashboard.component.css']
+})
+export class VideoDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
